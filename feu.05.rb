daa8b42c0a085c4420511b0e74c29126a76aab26
InesFe <103257625+InesFe@users.noreply.github.com>
2023-04-26 14:28:34 +0200

    Add files via upload

--- feu.05.rb
+++ feu.05.rb
@@ -65,32 +65,95 @@ def trouver_coordonées_sortie(lab_matrice) #renvoie les coordonées de différe
 	return coord_sortie
 end
 
+def trouver_la_sortie(lab_matrice, coord_entrée, coord_sortie)
+
+# Initialisation de la matrice de distance avec des valeurs à l'infini sauf pour l'entrée
+  distance_matrice = Array.new(lab_matrice.length) { Array.new(lab_matrice[0].length, Float::INFINITY) }
+  distance_matrice[coord_entrée[0]][coord_entrée[1]] = 0
+  
+  # Initialisation de la file pour le parcours en largeur
+  queue = [coord_entrée]
+  
+  while queue.length > 0
+    # On retire le prochain noeud de la file
+    noeud = queue.shift
+    
+    # On parcourt les noeuds adjacents
+    voisins = [[-1, 0], [0, 1], [1, 0], [0, -1]] # les 4 voisins possibles
+    voisins.each do |voisin|
+      row = noeud[0] + voisin[0]
+      col = noeud[1] + voisin[1]
+      
+      # Si le voisin est à l'intérieur de la matrice et non un obstacle
+      if row >= 0 && row < lab_matrice.length && col >= 0 && col < lab_matrice[0].length && lab_matrice[row][col] != 3
+        # Si la distance jusqu'à ce voisin est plus courte que la valeur précédente
+        if distance_matrice[row][col] > distance_matrice[noeud[0]][noeud[1]] + 1
+          distance_matrice[row][col] = distance_matrice[noeud[0]][noeud[1]] + 1
+          queue << [row, col]
+        end
+      end
+    end
+  end
+  
+  # Recherche de la sortie la plus proche
+  min_distance = Float::INFINITY
+  coord_sortie_proche = nil
+  coord_sortie.each_slice(2) do |sortie|
+    distance = distance_matrice[sortie[0]][sortie[1]]
+    if distance < min_distance
+      min_distance = distance
+      coord_sortie_proche = sortie
+    end
+    p coord_sortie_proche
+  end
+  
+  # Parcours du chemin pour tracer le chemin dans la matrice
+  chemin = []
+  compteur = 0
+  if min_distance < Float::INFINITY
+    noeud_courant = coord_sortie_proche
+    while distance_matrice[noeud_courant[0]][noeud_courant[1]] != 0
+    	chemin << noeud_courant
+      voisins = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+      voisins.each do |voisin|
+        row = noeud_courant[0] + voisin[0]
+        col = noeud_courant[1] + voisin[1]
+        
+        if lab_matrice[row][col] != coord_entrée[0][1]
+        if row >= 0 && row < lab_matrice.length && col >= 0 && col < lab_matrice[0].length && distance_matrice[row][col] == distance_matrice[noeud_courant[0]][noeud_courant[1]]-1
+          noeud_courant = [row, col]
+
+
+          lab_matrice[row][col] = 8
+          compteur += 1
+
+          
+        end 
+        end
+
+      end
+
+  end
+  
+end
+p lab_matrice
+
+return compteur -1
+end
+
+    
+
 #Parsing
 carte_laby = ARGV[0]
 
 #Appel des fonctions
 laby_matrice = faire_matrice(carte_laby)
-p laby_matrice
+#p laby_matrice
 coordonées_entrée = trouver_coordonées_entrée(laby_matrice)
 p coordonées_entrée
 coordonées_sortie = trouver_coordonées_sortie(laby_matrice)
 p coordonées_sortie
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+p trouver_la_sortie(laby_matrice, coordonées_entrée, coordonées_sortie)
 
 
 
